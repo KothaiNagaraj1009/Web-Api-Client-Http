dada62e19d7aae49eeeb1ed304423cb7eb05e109
KothaiNagaraj <87559312+KothaiNagaraj1009@users.noreply.github.com>
2021-08-27 04:07:42 -0700

    Add files via upload

---
+++ CoreApiDemoCall/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+//using MaheApi.Dto;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nancy.Json;
+
+namespace CoreApiDemoCall.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public string[] Get()
+        {
+            
+            return new string[] { "Mango", "Apple"};
+        }
+
+    }
+}
